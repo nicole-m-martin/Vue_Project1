cfae080e730384797804f9885e42f8bedebff4d5
Nicole Martin <n.martinpdx@gmail.com>
2022-04-03 10:49:22 -0700

    Set Up View CLI, Scoped & global css, Props, Emitting Custom Evnets, Click Event Modifiers

---
+++ vue-project2/modal-project/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>Welcome....</p>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+  </div>
+  <button @click.alt="toggleModal">open modal (option)</button>
+  <!-- Template Refs -->
+  <!-- <input type="text" ref="name" />
+  <button @click="handleClick">Click Me</button> -->
+</template>
+
+<script>
+import Modal from './components/Modal.vue';
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data() {
+    return {
+      title: 'My First Vue App :)',
+      header: 'Welcome!',
+      text: 'Join The Party Now',
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    // Template Refs
+    // handleClick() {
+    //   console.log(this.$refs.name);
+    //   this.$refs.name.classList.add('active');
+    //   this.$refs.name.focus();
+    // },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #eee;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
---
+++ vue-project2/modal-project/src/components/Modal.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- @click.self is a click event modifier  -->
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <h1>{{ header }}</h1>
+      <h2>{{ text }}</h2>
+      <p>modal content</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['header', 'text', 'theme'],
+  methods: {
+    closeModal() {
+      this.$emit('close');
+    },
+  },
+};
+</script>
+
+<style>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px;
+  background: white;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+/* h1,
+h2 {
+  color: black;
+} */
+
+.modal.sale {
+  background: crimson;
+  color: white;
+}
+.modal.sale h1 {
+  color: white;
+}
+</style>
