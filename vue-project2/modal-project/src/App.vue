c539278c206de0bbd1166447a640606ced898345
Nicole Martin <n.martinpdx@gmail.com>
2022-04-03 12:07:10 -0700

    use teleport

--- vue-project2/modal-project/src/App.vue
+++ vue-project2/modal-project/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>{{ title }}</h1>
   <p>Welcome....</p>
-  <div v-if="showModal">
+  <teleport to=".modal" v-if="showModal">
     <!-- SLOTS to inject a Template, with an open and closing Component-->
     <Modal theme="sale" @close="toggleModal">
       <!-- Custom Slot Name with v-slot -->
@@ -12,9 +12,9 @@
       <h1>Sale on Rollerblade's!</h1>
       <p>Get some blades at half price!</p>
     </Modal>
-  </div>
+  </teleport>
 
-  <div v-if="showModal2">
+  <teleport to=".modal" v-if="showModal2">
     <Modal @close="toggleModal2">
       <template v-slot:links2>
         <a href="#">sale items</a>
@@ -23,7 +23,7 @@
       <h1>Sale on Rollerskates!</h1>
       <p>Get some skates at half price!</p>
     </Modal>
-  </div>
+  </teleport>
 
   <button @click="toggleModal">open modal (option)</button>
   <button @click="toggleModal2">open 2nd modal</button>
@@ -54,7 +54,8 @@ export default {
 </script>
 
 <style>
-#app {
+#app,
+.modal {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
