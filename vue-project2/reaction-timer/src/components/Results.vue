61738d2f845bac18ec60cce34d9d1afeefcaa077
Nicole Martin <n.martinpdx@gmail.com>
2022-04-03 21:53:16 -0700

    reaction game porject

---
+++ vue-project2/reaction-timer/src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction Time: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Fastest!';
+    } else if (this.score < 400) {
+      this.rank = 'Fast!';
+    } else {
+      this.rank = 'So Slow!';
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4rem;
+  color: yellowgreen;
+  font-weight: bold;
+}
+</style>
