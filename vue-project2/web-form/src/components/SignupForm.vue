74924cfbdeac2766b30936719be106ac66d98373
Nicole Martin <n.martinpdx@gmail.com>
2022-04-05 20:36:55 -0700

    start signup formpractice

---
+++ vue-project2/web-form/src/components/SignupForm.vue
@@ -0,0 +1,127 @@
+<template>
+  <form>
+    <label>Email: </label>
+    <input type="email" required v-model="email" />
+
+    <label>Password: </label>
+    <input type="password" required v-model="password" />
+
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="manager">Project Manager</option>
+    </select>
+
+    <label>Skills:</label>
+    <input type="text" v-model="tempSkill" @keyup="addSkill" />
+    <div v-for="skill of skills" :key="skill" class="pill">
+      {{ skill }}
+    </div>
+
+    <div class="terms">
+      <input type="checkbox" required v-model="terms" />
+      <label>Accept terms and conditions</label>
+    </div>
+
+    <!-- multiple checkboxes -->
+    <!-- <div>
+      <input type="checkbox" value="nicole" v-model="names" />
+      <label>Nicole</label>
+    </div>
+
+    <div>
+      <input type="checkbox" value="cooper" v-model="names" />
+      <label>Cooper</label>
+    </div>
+
+    <div>
+      <input type="checkbox" value="ruby" v-model="names" />
+      <label>Ruby</label>
+    </div> -->
+  </form>
+
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+  <p>Checked: {{ terms }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      role: '',
+      terms: false,
+      // names: [],
+      tempSkill: '',
+      skills: [],
+    };
+  },
+  methods: {
+    addSkill(e) {
+      if (e.key === ',' && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill);
+        }
+
+        this.tempSkill = '';
+      }
+    },
+    deleteSkill() {},
+  },
+};
+</script>
+
+<style>
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+
+label {
+  color: #aaa;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.6rem;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+
+input,
+select {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+input[type='checkbox'] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
+}
+
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
+}
+</style>
