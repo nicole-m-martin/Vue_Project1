2c7aacf8d4a49e7b46d74a94037b6244c1cb0666
Nicole Martin <n.martinpdx@gmail.com>
2022-04-09 18:51:37 -0700

    add more fetures to form

--- vue-project2/web-form/src/components/SignupForm.vue
+++ vue-project2/web-form/src/components/SignupForm.vue
@@ -1,10 +1,11 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Email: </label>
     <input type="email" required v-model="email" />
 
     <label>Password: </label>
     <input type="password" required v-model="password" />
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
     <label>Role:</label>
     <select v-model="role">
@@ -15,12 +16,16 @@
     <label>Skills:</label>
     <input type="text" v-model="tempSkill" @keyup="addSkill" />
     <div v-for="skill of skills" :key="skill" class="pill">
-      {{ skill }}
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
     </div>
 
     <div class="terms">
       <input type="checkbox" required v-model="terms" />
       <label>Accept terms and conditions</label>
+    </div>
+
+    <div class="submit">
+      <button>Create Account</button>
     </div>
 
     <!-- multiple checkboxes -->
@@ -57,6 +62,7 @@ export default {
       // names: [],
       tempSkill: '',
       skills: [],
+      passwordError: '',
     };
   },
   methods: {
@@ -69,7 +75,18 @@ export default {
         this.tempSkill = '';
       }
     },
-    deleteSkill() {},
+    deleteSkill(skill) {
+      this.skills = this.skills.filter((item) => {
+        return skill !== item;
+      });
+    },
+    handleSubmit() {
+      //  validate password
+      this.passwordError =
+        this.password.length > 5
+          ? ''
+          : 'Password must be at least 6 chars long';
+    },
   },
 };
 </script>
@@ -123,5 +140,18 @@ input[type='checkbox'] {
   font-weight: bold;
   color: #777;
   cursor: pointer;
+}
+
+button {
+  background: #09be8e;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+
+.submit {
+  text-align: center;
 }
 </style>
